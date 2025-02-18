745dd9258009d8e68a5c3e8f819732f6db7b8059
Lebohang <lebohangmokhwibitjane@gmail.com>
2025-02-18 21:08:23 +0200

    tabs

--- main.py
+++ main.py
@@ -12,7 +12,35 @@ class Pomodoro:
         self.root.geometry("600x300")
         self.root.title("Pomodoro Timer")
         # self.root.tk.call("wm","iconphotot", self.root._w, PhotoImage(file="file.png"))
+
+        self.s = ttk.Style()
+        self.s.configure("TNotebook.Tab", font=("URW Gothic", 16))
+        self.s.configure("TButton", font=("URW Gothic", 16))
+
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill="both",pady=10, expand=True)
+
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Short Break")
+        self.tabs.add(self.tab3, text="Long Break")
+
         self.root.mainloop()
+
+    def start_timer(self):
+        pass
+
+    def start_timer_thread(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def skip(self):
+        pass
 
 if __name__ == '__main__':
     Pomodoro()
